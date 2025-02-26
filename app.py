8f5388e554deab8dd1cf9e27239ada1aafe05df3
Omkar31415 <vullaganti.omkar.3@gmail.com>
2025-02-26 17:34:34 -0600

    Test1

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from flask import Flask,request,render_template
-import numpy as np
-import pandas as pd
-
-from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
@@ -38,4 +34,4 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
